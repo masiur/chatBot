967f640fb442e6e5ae83501971dce7f52b7783ad
Masiur Rahman Siddiki <masiur.sust@csesociety.org>
2016-10-23 02:20:49 +0600

    jjj

--- app/Http/Controllers/ChatbotController.php
+++ app/Http/Controllers/ChatbotController.php
@@ -21,19 +21,19 @@ class ChatbotController extends Controller
         // return \Messenger::startConversation();
 
 
-        $access_token = env('token');
-        $verify_token = env('verify_token');
-        $hub_verify_token = null;
+        // $access_token = env('token');
+        // $verify_token = env('verify_token');
+        // $hub_verify_token = null;
          
-        if(isset($_REQUEST['hub_challenge'])) {
-            $challenge = $_REQUEST['hub_challenge'];
-            $hub_verify_token = $_REQUEST['hub_verify_token'];
-        }
+        // if(isset($_REQUEST['hub_challenge'])) {
+        //     $challenge = $_REQUEST['hub_challenge'];
+        //     $hub_verify_token = $_REQUEST['hub_verify_token'];
+        // }
          
          
-        if ($hub_verify_token === $verify_token) {
-            echo $challenge;
-        }
+        // if ($hub_verify_token === $verify_token) {
+        //     echo $challenge;
+        // }
 
 
 
@@ -42,25 +42,25 @@ class ChatbotController extends Controller
         $sender = $input['entry'][0]['messaging'][0]['sender']['id'];
         $message = $input['entry'][0]['messaging'][0]['message']['text'];
 
-        // $demo = new Answer();
-        // $demo->answer = $message;
-        // $demo->save();
+        $demo = new Answer();
+        $demo->answer = $message;
+        $demo->save();
 
 
         /**
          * Some Basic rules to validate incoming messages
          */
-        // if(preg_match('[time|current time|now]', strtolower($message))) {
+        if(preg_match('[time|current time|now]', strtolower($message))) {
 
-        //     // Make request to Time API
-        //     ini_set('user_agent','Mozilla/4.0 (compatible; MSIE 6.0)');
-        //     $result = file_get_contents("http://www.timeapi.org/utc/now?format=%25a%20%25b%20%25d%20%25I:%25M:%25S%20%25Y");
-        //     if($result != '') {
-        //         $message_to_reply = $result;
-        //     }
-        // } else {
-        //     $message_to_reply = 'Huh! what do you mean?';
-        // }
+            // Make request to Time API
+            ini_set('user_agent','Mozilla/4.0 (compatible; MSIE 6.0)');
+            $result = file_get_contents("http://www.timeapi.org/utc/now?format=%25a%20%25b%20%25d%20%25I:%25M:%25S%20%25Y");
+            if($result != '') {
+                $message_to_reply = $result;
+            }
+        } else {
+            $message_to_reply = 'Huh! what do you mean?';
+        }
         // print $message_to_reply;
 
         //API Url
@@ -76,41 +76,29 @@ class ChatbotController extends Controller
                     "id":"'.$sender.'"
                 },
                 "message":{
-                    "text":"Huh! what do you mean?"
+                    "text":"'.$message_to_reply.'"
                 }
             }';
-
-
-
-            $options_header = array ( //Necessary Headers
-                    'http' => array(
-                    'method' => 'POST',
-                    'content' => json_encode($jsonData),
-                    'header' => "Content-Type: application/json\n"
-                )
-            );
-            $context = stream_context_create($options_header);
-            file_get_contents("https://graph.facebook.com/v2.6/me/messages?access_token=$access_token",false,$context);
 
             
 
             //Encode the array into JSON.
-            // $jsonDataEncoded = $jsonData;
+            $jsonDataEncoded = $jsonData;
 
-            // //Tell cURL that we want to send a POST request.
-            // curl_setopt($ch, CURLOPT_POST, 1);
+            //Tell cURL that we want to send a POST request.
+            curl_setopt($ch, CURLOPT_POST, 1);
 
-            // //Attach our encoded JSON string to the POST fields.
-            // curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
+            //Attach our encoded JSON string to the POST fields.
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
 
-            // //Set the content type to application/json
-            // curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-            // //curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));
+            //Set the content type to application/json
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+            //curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));
 
-            // //Execute the request
-            // if(!empty($input['entry'][0]['messaging'][0]['message'])){
-            //     $result = curl_exec($ch);
-            // }
+            //Execute the request
+            if(!empty($input['entry'][0]['messaging'][0]['message'])){
+                $result = curl_exec($ch);
+            }
 
 
 
